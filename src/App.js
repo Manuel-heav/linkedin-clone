d5979e9c21d986334ae559ca6bab5e4c263a9c03
manuel-heav <manuelheav32@gmail.com>
2022-12-15 18:51:51 +0300

    working on the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a linkedIn clone!!!</h1>
+
+      {/* Header */}
+      <Header />
+      {/* App body */}
+    
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
