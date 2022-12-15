4be3ccee3c1ff8dc20ae07777092b1866ef0d0c9
manuel-heav <manuelheav32@gmail.com>
2022-12-15 19:25:26 +0300

    working on the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import './Header.css';
 const Header = () => {
   return (
     <div className="header">
-        <h1>This is the header</h1>
+        <div className="header__left">
+            <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" />
+
+        <div className="header__search">
+            <SearchIcon />
+            <input type="text" />
+        </div>
+        </div>
+
+        <div className="header__right"></div>
     </div>
   )
 }
