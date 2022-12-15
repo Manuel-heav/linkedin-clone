d1a58a666825351b6e23024074142c261e3f246e
manuel-heav <manuelheav32@gmail.com>
2022-12-15 18:38:10 +0300

    initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuw5K2h1JnYkJgbskKN4UhqjIZgMP5xdw",
+  authDomain: "linkedin-clone-7b7b8.firebaseapp.com",
+  projectId: "linkedin-clone-7b7b8",
+  storageBucket: "linkedin-clone-7b7b8.appspot.com",
+  messagingSenderId: "769414044645",
+  appId: "1:769414044645:web:22d79c8e4bcf3102b42d0c",
+  measurementId: "G-S219JX67XW"
+};
