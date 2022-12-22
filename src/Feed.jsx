f6082650194c024be5d5913d72bbe0538b17ccb5
manuel-heav <manuelheav32@gmail.com>
2022-12-22 20:07:06 +0300

    working on feed

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/Event';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+
+import InputOption from './InputOption';
 const Feed = () => {
   return (
     <div className="feed">
@@ -11,6 +17,13 @@ const Feed = () => {
                     <input type="text" />
                     <button type="submit">Send</button>
                 </form>
+            </div>
+
+            <div className="feed__inputOptions">
+                <InputOption title="Photo" Icon={ImageIcon} color="#70b5f9"/>
+                <InputOption title="Videos" Icon={SubscriptionsIcon} color="#e7a33e"/>
+                <InputOption title="Event" Icon={EventNoteIcon} color="#c0cbcd"/>
+                <InputOption title="Write" Icon={CalendarViewDayIcon} color="#7fc15e"/>
             </div>
         </div>
     </div>
